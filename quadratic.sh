10a9f05723a63b0a61d7f1dfa297df8073ebfecb
jmcorbera <jmcorbera@gmail.com>
2021-10-12 15:03:31 -0300

    Update quadratic.sh

--- quadratic.sh
+++ quadratic.sh
@@ -1,3 +1,3 @@
-sudo nasm -f elf32 funcionCuadratica.asm -o funcionCuadratica.o
-sudo gcc -m32 funcionCuadratica.c funcionCuadratica.o -o main
+sudo nasm -f elf32 quadratic.asm -o quadratic.o
+sudo gcc -m32 quadratic.c quadratic.o -o main
 ./main
